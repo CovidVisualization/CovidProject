0550e2bd151dc067316970ab8bafde4a25690fc6
Mansea <6210742034@student.tu.ac.th>
2021-08-27 13:56:46 +0700

    change data table

--- src/app/check-covid/check-covid.component.ts
+++ src/app/check-covid/check-covid.component.ts
@@ -1,42 +1,105 @@
-import {Component,ViewChild,AfterViewInit} from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface UserData {
   name: string;
   position: number;
-  phone: number;
-  email: string;
+  district: string;
+  province: string;
+  type: string;
+  typeEnter: string;
+  price: string;
+  location: string;
 }
 
 const ELEMENT_DATA: UserData[] = [
-  {position: 1, name: 'Mahesh', phone: 8585747, email: 'Hmahesh@wwe.com'},
-  {position: 2, name: 'Ramesh', phone: 855588, email: 'rames@wei.com'},
-  {position: 3, name: 'Rahul', phone: 99696, email: 'newi@ssw.com'},
-  {position: 4, name: 'Minu', phone: 965555, email: 'nieue@ww.com'},
-  {position: 5, name: 'Mishu', phone: 9125477, email: 'mishu@wa.com'},
-  {position: 6, name: 'Mayank', phone: 914755, email: 'mayku@wido.com'},
-  {position: 7, name: 'Hiren', phone: 9425658, email: 'hortet@wwi.com'},
-  {position: 8, name: 'Alex', phone: 985224, email: 'fheuuw@ww.com'},
-  {position: 9, name: 'Joy', phone: 963544, email: 'itituyd@ww.com'},
-  {position: 10, name: 'Ashok', phone: 854444, email: 'jeje@woodd.com'},
+  {
+    position: 1,
+    name: 'โรงพยาบาลยอนฮี',
+    district: 'กรุงเทพมหานคร',
+    province: 'ดอนเมือง',
+    type: 'CPR',
+    typeEnter: 'Drive-Thu',
+    price: '3000',
+    location: 'google map',
+  },
+  {
+    position: 2,
+    name: 'โรงพยาบาล Mansea',
+    district: 'กรุงเทพมหานคร',
+    province: 'ลาดพร้าว',
+    type: 'CPR',
+    typeEnter: 'Drive-Thu',
+    price: '1500',
+    location: 'google map',
+  },
+  {
+    position: 3,
+    name: 'โรงพยาบาล Mansea',
+    district: 'กรุงเทพมหานคร',
+    province: 'ลาดพร้าว',
+    type: 'CPR',
+    typeEnter: 'Drive-Thu',
+    price: '1500',
+    location: 'google map',
+  },
+  {
+    position: 4,
+    name: 'โรงพยาบาล Mansea',
+    district: 'กรุงเทพมหานคร',
+    province: 'ลาดพร้าว',
+    type: 'CPR',
+    typeEnter: 'Drive-Thu',
+    price: '1500',
+    location: 'google map',
+  },
+  {
+    position: 5,
+    name: 'โรงพยาบาล Mansea',
+    district: 'กรุงเทพมหานคร',
+    province: 'ลาดพร้าว',
+    type: 'CPR',
+    typeEnter: 'Drive-Thu',
+    price: '1500',
+    location: 'google map',
+  },
+  {
+    position: 6,
+    name: 'โรงพยาบาล Mansea',
+    district: 'กรุงเทพมหานคร',
+    province: 'ลาดพร้าว',
+    type: 'CPR',
+    typeEnter: 'Drive-Thu',
+    price: '1500',
+    location: 'google map',
+  },
 ];
 
 /**
  * @title Table with filtering
  */
 @Component({
-   selector: 'app-check-covid',
+  selector: 'app-check-covid',
   templateUrl: './check-covid.component.html',
-  styleUrls: [ './check-covid.component.css' ]
+  styleUrls: ['./check-covid.component.css'],
 })
 export class CheckCovidComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'phone', 'email'];
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'district',
+    'province',
+    'type',
+    'typeEnter',
+    'price',
+    'location',
+  ];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-   @ViewChild(MatSort) sort: MatSort;
-   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
