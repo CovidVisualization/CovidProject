f312d341010cb1ca7501cd3083eed3d149a96c1e
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2021-08-28 17:30:46 +0700

    chart completed

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -11,77 +11,61 @@ import { HttpClient } from '@angular/common/http';
 
 export class ChartComponent implements OnInit {
 
-  date_lebels: string[] = [];
-  date_newCase: string[] = [];
-  temp1 = ['Mon', 'Tue', 'Wed', 'Thr', 'Thu', 'Fri', 'Sat', 'Sun'];
+  type : any;
+  data : any;
+  options : any;
 
-
-  type = "line";
-  data = {};
-  
-  options = {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true
-        
-      }
-    }
-  }
-
-  
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('http://25.58.26.212/coviD/api/covid/totalWeek').subscribe(data => {
 
-    // for(let i = 0; i <= 7; i++){
-    //   this.date_lebels.push(data[i].date)
-    // }
-
+      let date_lebels: string[] = [];
+      let date_newCase: string[] = [];
 
       Object.entries(data).forEach(
-        ([key, value]) => this.date_lebels.push(value["date"])
+        ([key, value]) => date_lebels.push(value["date"])
       );
 
       Object.entries(data).forEach(
-        ([key, value]) => this.date_newCase.push(value["newCase"])
+        ([key, value]) => date_newCase.push(value["newCase"])
       );
 
-      console.log(this.date_lebels);
-      console.log(this.date_newCase);
-
+      this.type = "line";
+      this.data = {
+        labels: date_lebels,
+        datasets: [{
+          label: '# of infected',
+          data: date_newCase,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+      this.options = {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+            
+          }
+        }
+      }
     })
-
-    this.data = {
-      labels: this.date_lebels,
-      datasets: [{
-        label: '# of infected',
-        data: this.date_newCase,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    }
-
-
-
-
-  }
-
+  } //end of ngOnInit
 }
 
