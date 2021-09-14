4d3abbab836e031d565127476bcb5c3e95c566de
P2OM <heammapoom@gmail.com>
2021-09-14 21:53:19 +0700

    date, data != 0 (infected-section)

--- src/app/infected-section/infected-section.component.ts
+++ src/app/infected-section/infected-section.component.ts
@@ -13,6 +13,7 @@ export class InfectedSectionComponent implements OnInit {
   amountHealing = "";
   amountHealed = "";
   amountDeath = "";
+  informationDate = "";
 
   constructor(private http: HttpClient) { }
 
@@ -20,19 +21,34 @@ export class InfectedSectionComponent implements OnInit {
     this.http.get<any>('http://25.58.26.212/covid/api/covid/totalcovid').subscribe(data => {
       
       var intTotal: number = +data[0].totalnew;
-      this.amountInfected = intTotal.toLocaleString();
-
+      if (intTotal.toLocaleString() != "0") {
+        this.amountInfected = intTotal.toLocaleString();
+      }
+      
       var intTotalToday: number = +data[0].newCase;
-      this.amountInfectedToday = intTotalToday.toLocaleString();
-
+      if (intTotalToday.toLocaleString() != "0") {
+        this.amountInfectedToday = intTotalToday.toLocaleString();
+      }
+      
       var intHealing: number = +data[0].healing
-      this.amountHealing = intHealing.toLocaleString()
-
+      if (intHealing.toLocaleString() != "0") {
+        this.amountHealing = intHealing.toLocaleString()
+      }
+      
       var intHealed: number = +data[0].healed
-      this.amountHealed = intHealed.toLocaleString()
+      if (intHealed.toLocaleString() != "0") {
+        this.amountHealed = intHealed.toLocaleString()
+      }
 
       var intDeath: number = +data[0].dead
-      this.amountDeath = intDeath.toLocaleString()
+      if (intDeath.toLocaleString() != "0") {
+        this.amountDeath = intDeath.toLocaleString()
+      }
+
+      var charDate: any = data[0].date
+      if (charDate.toLocaleString() != "0") {
+        this.informationDate = charDate.toLocaleString()
+      }
 
     })
   }
