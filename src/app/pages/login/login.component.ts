bb2081b32e2a882cf0c54cd472218b8d76e19c33
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2021-10-06 15:48:02 +0700

    Login register update again not done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,17 +8,45 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  displayLogin = true;
+  displayRegister = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+
+  LoginButtonClick() {
+    this.displayLogin = true;
+    this.displayRegister = false;
+  }
+
+  RegisterButtonClick() {
+    this.displayLogin = false;
+    this.displayRegister = true;
+  }
+
+  wantToLogin() {
+    return this.displayLogin
+  }
+
+  wantToRegister() {
+    return this.displayRegister
+  }
+
   onLogin(loginFrom: NgForm): void {
-    console.log(loginFrom.value);
-    const token = loginFrom.value;
-    console.log(token.userName);
-    console.log(token.password);
+    const loginToken = loginFrom.value;
+    console.log(loginToken.userEmail);
+    console.log(loginToken.password);
+  }
+
+  onRegister(registerFrom: NgForm): void {
+    const registerToken = registerFrom.value;
+    console.log(registerToken);
+    // console.log(registerToken.userEmail);
+    // console.log(registerToken.password);
   }
 
 }
-
